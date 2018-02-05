24deac2f768c779dbcd91070d032928f772be3eb
石雪峰 <shixuefeng@le.com>
2018-02-05 17:58:15 +0100

    mount docker.sock
    Signed-off-by: 石雪峰 <shixuefeng@le.com>

--- build-a-java-app-with-maven/env-init.sh
+++ build-a-java-app-with-maven/env-init.sh
@@ -4,4 +4,4 @@
 docker run -d -p 3000:3000 --name git-server cendrierdocker/git-server:v1
 
 # Run jenkins server
-docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-server --link git-server:git-server cendrierdocker/jenkins-server:v1
+docker run -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock --name jenkins-server --link git-server:git-server cendrierdocker/jenkins-server:v1
