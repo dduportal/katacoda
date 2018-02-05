f905e3adaa3e369477050ed5ceb56d37bdf6b4b3
石雪峰 <shixuefeng@le.com>
2018-02-05 16:09:29 +0100

    Add jenkins server dockerfile and active in initial script
    Signed-off-by: 石雪峰 <shixuefeng@le.com>

--- build-a-java-app-with-maven/env-init.sh
+++ build-a-java-app-with-maven/env-init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Run git server
 docker run -d -p 3000:3000 --name git-server cendrierdocker/git-server:v1
+
+# Run jenkins server
+docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-server cendrierdocker/jenkins-server:v1
